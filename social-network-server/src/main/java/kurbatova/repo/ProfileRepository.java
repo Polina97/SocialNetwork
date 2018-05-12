d33f0b2c0c430e5a820d644ba93b4e4711ec1068
Aliaksei Piskun <aliaksei_piskun@senla.eu>
2018-05-13 01:22:26 +0300

    repo changes, native query

--- social-network-server/src/main/java/kurbatova/repo/ProfileRepository.java
+++ social-network-server/src/main/java/kurbatova/repo/ProfileRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import kurbatova.model.Profile;
-import kurbatova.model.User;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+
+	@Query(value = "SELECT * FROM profile p where p.user_id = :userId LIMIT 1", 
+	    nativeQuery=true
+	)
+	Profile getProfileByUserId(@Param("userId") String userId);
 	
-	@Query("SELECT p.profileId, p.firstName, p.lastName, p.birthDay FROM Profile p where p.user = :user")
-	Profile getProfileById(@Param("user") User user);
 }
